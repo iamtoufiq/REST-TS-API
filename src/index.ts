79a056b83903b19f5fc805792e8eed882e11ac4c
Toufiq Choudhari <iamtoufiq2@gmail.com>
2025-02-16 13:54:57 +0530

    setup the inital server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello console to check thse setuptaa")
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app =express()
+const PORT = 8080
+
+app.use(cors({
+    credentials:true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server =http.createServer(app)
+
+server.listen(PORT , ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
